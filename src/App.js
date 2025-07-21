6143117378303da0f532917e91674ac32f4d0fa9
Manit Devra <manitdevra123@gmail.com>
2025-07-21 14:28:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+
+const allbrands = [
+  { id: 1, name: "puma" },
+  { id: 2, name: "zara" },
+  { id: 3, name: "armani" },
+  { id: 4, name: "gucci" },
+  { id: 5, name: "dior" },
+];
 
 function App() {
+  const [brand, setBrand] = useState(allbrands);
+
+  const onsearch = (event) => {
+    const filteredbrands = allbrands.filter((brand) =>
+      brand.name.includes(event.target.value.toLowerCase())
+    );
+    setBrand(filteredbrands);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={onsearch} placeholder="search here........"></input>
+      <ul>
+        {brand.map((brand) => (
+          <li key={brand.id}>{brand.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
